ed5b3df3a7748bc38e4c0176cb07d338b6b5c15c
jacksonperplexitypro-glitch <jacksonperplexitypro@gmail.com>
2026-02-09 22:24:20 +0800

    1

---
+++ js/uag-js-1815.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-500aa577' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-9e6d328e' );
+});
+ });
